39bdaed3d0bcb905b78a4b99791505c400f310ba
riad_zakir@yahoo.com <riad_zakir@yahoo.com>
2021-08-26 14:04:51 +0600

    Added Self signed certificate for production enviorment

--- src/Services/Identity/identity.API/Startup.cs
+++ src/Services/Identity/identity.API/Startup.cs
@@ -19,16 +19,20 @@ using identity.API.Repositories.IdentityService;
 using identity.API.Repositories.Profile;
 using identity.API.Data.Feature;
 using identity.API.Repositories.TenantIdentity;
-using Microsoft.Extensions.Logging;
 using identity.API.Data.Role;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using Microsoft.IdentityModel.Tokens;
 
 namespace identity.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IWebHostEnvironment _environment;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _environment = env;
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
         }
 
@@ -48,15 +52,40 @@ namespace identity.API
             });
 
             // AddDeveloperSigningCredential not something we want to use in a production environment;
-            // For the production environment, you should use the AddSigningCredentials method and provide a valid certificate.
-            services.AddIdentityServer()
-                .AddProfileService<ProfileService>()
-                .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
-                .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
-                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
-                //.AddTestUsers(InMemoryConfig.GetUsers())
-                //.AddInMemoryClients(InMemoryConfig.GetClients())
-                .AddDeveloperSigningCredential();
+            // For the production environment, you should use the AddSigningCredentials method and provide a valid certificate.           
+
+            //services.AddIdentityServer()
+            //.AddProfileService<ProfileService>()
+            //.AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
+            //.AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+            //.AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources());
+            //.AddTestUsers(InMemoryConfig.GetUsers())
+            //.AddInMemoryClients(InMemoryConfig.GetClients())
+            //.AddSigningCredential();
+            //.AddDeveloperSigningCredential();
+
+            if (_environment.IsDevelopment())
+            {
+                services.AddIdentityServer()
+                    .AddProfileService<ProfileService>()
+                    .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
+                    .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+                    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                    .AddDeveloperSigningCredential();
+            }
+            else
+            {
+                // create certificate, Does not exist atm.
+                X509Certificate2 rsaCertificate = new X509Certificate2(Path.Combine(_environment.ContentRootPath, "certificate.p12"), "aee8f4215062458c9654ea954dfc1e6a");
+
+                services.AddIdentityServer()
+                    .AddProfileService<ProfileService>()
+                    .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
+                    .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+                    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                    .AddSigningCredential(rsaCertificate);
+            }
+
 
             services.AddAuthentication("Bearer").AddJwtBearer("Bearer", opt =>
            {
